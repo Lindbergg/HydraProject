45b400ec132cc9f1a5bb4a2e182fb75dffabf3c9
mjesper90 <m.jesper90@gmail.com>
2025-05-17 17:47:25 +0200

    updated hydra values

--- HydraValues.py
+++ HydraValues.py
@@ -1,2 +1,15 @@
 class HydraValues:
     def __init__(self):
+        self.column1 = "Common"
+        self.column2 = "Uncommon"
+        self.column3 = "Rare"
+        self.column4 = "Legendary"
+
+        self.herpderp = [
+            [25, 40, 70, 150],
+            [35, 50, 85, 190],
+            [40, 65, 110, 280],
+        ]
+        
+    def getvalue(self, x, y):
+        return self.herpderp[y][x]
